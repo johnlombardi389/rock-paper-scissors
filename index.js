3dcb26919d0d0a5984e1d942de7668fbeac0cee6
johnlombardi389 <lombardij38@gmail.com>
2022-07-19 19:16:44 -0400

    Separated computer and player choices into their own functions

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
+// Computer hand choice
 function getComputerChoice() {
-  let cpuHand = ["Rock", "Paper", "Scissors"];
+  let cpuHand = ["ROCK", "PAPER", "SCISSORS"];
   let random = Math.floor(Math.random() * cpuHand.length);
   console.log("CPU: " + cpuHand[random]);
 }
 
-function game(playerSelection, cpuSelection){
-  playerSelection = prompt("Rock, Paper, or Scissors?");
-  cpuSelection = getComputerChoice();
-
-  console.log("Player: " + playerSelection);
+// Player hand choice
+function playerChoice() {
+  let choice = prompt("Rock, Paper, or Scissors?");
+  while (choice == null) {
+    choice = prompt("Rock, Paper, or Scissors?");
+  }
+  choice = choice.toUpperCase();
 }
-
-game();
