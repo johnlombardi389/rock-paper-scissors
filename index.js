0eeb5e6e17d3345a39032b4d50854dc2499bfab6
johnlombardi389 <lombardij38@gmail.com>
2022-07-28 15:31:23 -0400

    Added winner function to declare player the winner

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ let cpuScore = 0;
 const playerScore_span = document.getElementById('player-score');
 const cpuScore_span = document.getElementById('cpu-score');
 const scoreboard_div = document.querySelector('.scoreboard');
-const result_div = document.querySelector('.result');
+const result_div = document.querySelector('.result > p');
 const rock_btn = document.getElementById('rock');
 const paper_btn = document.getElementById('paper');
 const scissors_btn = document.getElementById('scissors');
@@ -16,23 +16,38 @@ function getCpuChoice() {
   return choices[randomNum];
 }
 
+function winner(playerChoice, cpuChoice) {
+  playerScore++;
+  playerScore_span.innerHTML = playerScore;
+  result_div.innerHTML = playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1) + " > " + cpuChoice.charAt(0).toUpperCase() + cpuChoice.slice(1) + ". You win!";
+}
+
+function loser(playerChoice, cpuChoice) {
+  cpuScore++;
+  cpuScore_span.innerHTML = cpuScore;
+}
+
+function tie(playerChoice, cpuChoice) {
+  console.log("TIE");
+}
+
 function playRound(playerChoice) {
   const cpuChoice = getCpuChoice();
   switch (playerChoice + cpuChoice) {
     case "rockscissors":
     case "paperrock":
     case "scissorspaper":
-      console.log("Player wins!");
+      winner(playerChoice, cpuChoice);
       break;
     case "rockpaper":
     case "paperscissors":
     case "scissorsrock":
-      console.log("CPU wins!");
+      loser(playerChoice, cpuChoice);
       break;
     case "rockrock":
     case "paperpaper":
     case "scissorsscissors":
-      console.log("Tie!");
+      tie(playerChoice, cpuChoice);
       break;
   }
 }
